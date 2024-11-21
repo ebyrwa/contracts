a33d53060422302c7ea803acc94448f159a35510
Erik Byrwa <Eri072219@paycor.com>
2024-11-21 14:45:35 -0500

    adds transaction

---
+++ Contracts/Transaction.cs
@@ -0,0 +1,10 @@
+﻿namespace Contracts
+{
+    public class Transaction
+    {
+        public decimal Amount { get; set; }
+        public DateTimeOffset PostingDate { get; set; }
+        public string ReferenceNumber { get; set; }
+        public Guid TransactionId { get; set; }
+    }
+}
